68d689ede6f253296c68f3df98cc2d1c6be50111
Zan <nurdinizzan@gmail.com>
2022-03-06 01:35:20 +0700

    feat(page): init fetch data using SteinStore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.scss';
+import SteinStore from 'stein-js-client';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const TOTAL_DATA = 124;
+
+  const store = new SteinStore("https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4");
+
+  useEffect(() => {
+    store.read("list").then(response => {
+      setData(response)
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+        {console.log(data, 'data')}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
